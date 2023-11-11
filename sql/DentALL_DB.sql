758b644c44d31d1da57a7148e519afb4ba6d64ee
kbaljak <karlo.baljak@fer.hr>
2023-11-11 23:00:47 +0100

    Skripta radi

--- sql/DentALL_DB.sql
+++ sql/DentALL_DB.sql
@@ -1,26 +1,42 @@
+CREATE TABLE Town( --table
+	TownID bigserial primary key,
+	townName varchar(35)
+);
+
+CREATE TABLE UserRole( --table
+	RoleID smallint primary key,
+	roleName varchar(30)
+);
+
+CREATE TABLE Treatment( --table
+	TreatmentID bigserial primary key,
+	description text
+);
+
+CREATE TABLE AccomodationType( --table
+	TypeID smallint primary key,
+	desciption text
+);
+
+CREATE TABLE VehicleType( --table
+	TypeID smallint primary key,
+	description text
+);
+
+CREATE TABLE Equipped( --table
+	EquippedID smallint primary key,
+	description text
+);
+
 CREATE TABLE Clinic( --table
 	ClinicID bigserial primary key,
 	clinicName varchar(90),
 	latitude decimal(8,6),
 	longitude decimal(9,6),
 	clinicAddress varchar (95),
+	TownID bigserial,
 	foreign key (TownID) references Town(TownID)
-),
-
-CREATE TABLE clinicAccomodation( --relation
-	foreign key (ClinicID) references Clinic(ClinicID),
-	foreign key (AccomodationID) references Accomodation(AccomodationID)
-),
-
-CREATE TABLE clinicTransporter( --relation
-	foreign key (ClinicID) references Clinic(ClinicID),
-	foreign key (TransporterID) references Transporter(TransporterID)
-),
-
-CREATE TABLE Town( --table
-	TownID bigserial primary key,
-	townName varchar(35)
-),
+);
 
 CREATE TABLE AdminUser( --table
 	UserID bigserial primary key,
@@ -29,23 +45,7 @@ CREATE TABLE AdminUser( --table
 	lastname varchar(30),
 	phone varchar(20),
 	email varchar(60)
-),
-
-CREATE TABLE Credentials( --table
-	username varchar(50),
-	pass BINARY(64),
-	foreign key (UserID) references AdminUser(UserID)
-),
-
-CREATE TABLE assignedRole( --relation
-	foreign key(UserID) references AdminUser(UserID),
-	foreign key(RoleID) references Role(RoleID)
-),
-
-CREATE TABLE UserRole( --table
-	RoleID smallint primary key,
-	roleName varchar(30)
-),
+);
 
 CREATE TABLE Patient( --table
 	PatientID bigserial primary key,
@@ -54,96 +54,146 @@ CREATE TABLE Patient( --table
 	lastname varchar(30),
 	phone varchar(20),
 	email varchar(60),
-	redidenceAddress varchar(95),
-),
+	redidenceAddress varchar(95)
+);
 
-CREATE TABLE Treatment( --table
-	TreatmentID bigserial primary key,
-	description text
-),
-
-CREATE TABLE assigned( --realtion
-	foreign key (TreatmentID) references Treatment(TreatmentID),
-	foreign key (PatientID) references Patient(PatiendID)
-),
-
-CREATE TABLE PatientPlan( --relation
-	foreign key (TreatmentID) references Treatment(TreatmentID),
-	foreign key	(ClinicID) references Clinic(ClinicID),
-	foreign key (PatientID) references Patient(PatientID)
-),
-
-CREATE TABLE PatientPreferences( --table
+CREATE TABLE PatientArrival( --table
+	ArrivalID bigserial primary key,
+	PatientID bigserial,
+	TownID bigserial,
+	dateOfArrival timestamp,
+	dateOfDeparture timestamp,
 	foreign key (PatientID) references Patient(PatientID),
-	foreign key (TyepID) references AccomodationType(TypeID),
-	foreign key (EquippedID) references Equipped(EquippedID)
-),
+	foreign key (TownID) references Town(TownID)
+);
+
+CREATE TABLE Credentials( --table
+	username varchar(50),
+	pass varchar(25),
+	UserID bigserial,
+	foreign key (UserID) references AdminUser(UserID)
+);
 
 CREATE TABLE Accomodation( --table
 	AccomodationID bigserial primary key,
-	foreign key (TypeID) references AccomodationType(TypeID),
-	foreign key (EquippedID) references Equipped(EquippedID),
+	TypeID smallint,
+	EquippedID smallint,
 	latitude decimal(9,6),
 	longitude decimal(9,6),
 	address varchar(95),
-	foreign key (TownID) references Town(TownID),
-	active bit
-),
-
-CREATE TABLE AccomodationType( --table
-	TypeID smallint primary key,
-	desciption text
-),
-
-CREATE TABLE Equipped( --table
-	EquippedID smallint primary key,
-	description text
-),
+	TownID bigserial,
+	active bit,
+	foreign key (TypeID) references AccomodationType(TypeID),
+	foreign key (EquippedID) references Equipped(EquippedID),
+	foreign key (TownID) references Town(TownID)
+);
 
 CREATE TABLE AccomodationOccupied( --table
-	foreign key (PatientID) references Patient(PatientID),
-	foreign key (AccomodationID) references Accomodation(AccomodationID),
+	ID bigserial primary key,
+	PatientID bigserial,
+	AccomodationID bigserial,
 	datoFrom date,
-	dateTo date
-),
+	dateTo date,
+	foreign key (PatientID) references Patient(PatientID),
+	foreign key (AccomodationID) references Accomodation(AccomodationID)
+);
 
 CREATE TABLE Transporter( --table
 	TransporterID bigserial primary key,
 	organisationName varchar(70),
 	phone varchar(20),
 	address varchar(95),
-	foreign key(TownID) references Town(TownID),
-	active bit
-),
+	TownID bigserial,
+	active bit,
+	foreign key(TownID) references Town(TownID)
+);
 
 CREATE TABLE Vehicle( --table
 	VehicleID bigserial primary key,
 	capacity smallint,
+	TypeID smallint,
+	TransporterID bigserial,
+	active bit,
 	foreign key (TypeID) references VehicleType(TypeID),
-	foreign key (TransporterID) references Transporter(TransporterID),
-	active bit	
-),
-
-CREATE TABLE VehicleType( --table
-	TypeID smallint primary key,
-	description text
-),
+	foreign key (TransporterID) references Transporter(TransporterID)
+);
 
 CREATE TABLE VehicleOccupied( --table
-	foreign key (VehicleID) references Vehicle(VehicleID),
-	foreign key (PatiendID) references Patient(PatientID),
+	OrderID bigserial primary key,
+	VehicleID bigserial,
+	PatientID bigserial,
 	timeStart time,
-	timeEnd time
-),
-
-CREATE TABLE VehicleAvaliability( --table
+	timeEnd time,
 	foreign key (VehicleID) references Vehicle(VehicleID),
+	foreign key (PatientID) references Patient(PatientID)
+);
+
+CREATE TABLE VehicleSchedule( --table
+	VehicleID bigserial,
 	DOW smallint,
 	timeStart time,
-	timeEnd time	
-)
+	timeEnd time,
+	foreign key (VehicleID) references Vehicle(VehicleID)
+);
+
+CREATE TABLE assignedRole( --relation
+	UserID bigserial,
+	RoleID bigserial,
+	foreign key(UserID) references AdminUser(UserID),
+	foreign key(RoleID) references UserRole(RoleID)
+);
+
+CREATE TABLE clinicAccomodation( --relation
+	ClinicID bigserial,
+	AccomodationID bigserial,
+	foreign key (ClinicID) references Clinic(ClinicID),
+	foreign key (AccomodationID) references Accomodation(AccomodationID)
+);
+
+CREATE TABLE clinicTransporter( --relation
+	ClinicID bigserial,
+	TransporterID bigserial,
+	foreign key (ClinicID) references Clinic(ClinicID),
+	foreign key (TransporterID) references Transporter(TransporterID)
+);
+
+CREATE TABLE assigned( --realtion
+	TreatmentID bigserial,
+	PatientID bigserial,
+	foreign key (TreatmentID) references Treatment(TreatmentID),
+	foreign key (PatientID) references Patient(PatientID)
+);
+
+CREATE TABLE PatientPlan( --relation
+	TreatmentID bigserial,
+	ClinicID bigserial,
+	PatientID bigserial,
+	foreign key (TreatmentID) references Treatment(TreatmentID),
+	foreign key	(ClinicID) references Clinic(ClinicID),
+	foreign key (PatientID) references Patient(PatientID)
+);
+
+CREATE TABLE PatientPreferences( --table
+	PatientID bigserial,
+	TypeID smallint,
+	EquippedID smallint,
+	foreign key (PatientID) references Patient(PatientID),
+	foreign key (TypeID) references AccomodationType(TypeID),
+	foreign key (EquippedID) references Equipped(EquippedID)
+);
+
 
 --create users and groups
+
+CREATE ROLE auth WITH
+	LOGIN
+	NOSUPERUSER
+	NOCREATEDB
+	NOCREATEROLE
+	NOINHERIT
+	NOREPLICATION
+	CONNECTION LIMIT -1;
+	
 CREATE ROLE app_api WITH
 	NOLOGIN
 	NOSUPERUSER
