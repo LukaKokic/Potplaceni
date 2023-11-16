ca058cdf08dbeb5f8c234ae92afdc76377e84f32
LukaKokic <lk53973@fer.hr>
2023-11-16 12:51:35 +0100

    basic login interface

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import SignIn from './assets/Sign-in template/SignIn.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <SignIn />
   </React.StrictMode>,
 )
