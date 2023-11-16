529b66bbb6042753fea932c3fbe13be98c67de6a
kbaljak <karlo.baljak@fer.hr>
2023-11-16 21:40:43 +0100

    pseudo login

--- middleware/index.js
+++ middleware/index.js
@@ -1,30 +1,44 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const pool = require('./DBConfig');
+const passwordHasher = require('./passwordhassher');
 const cors = require('cors');
-dotenv.config();
-//const poolParameters = require('databaseConnection.json')
 const app = express();
+
+dotenv.config();
 app.use(express.json());
 app.use(cors());
+
 const port = 8080;
 
-
+//TODO connect this to front and greet user with login page
 app.get('/', (req, res) => {
   res.send('Hello World')
 });
 
-app.get('/adminuser', async(req, res) => {
+//function do be called when login button has been pressed
+function loginFunction(username, password){
+  hashedPassword = passwordHasher(password);
+  //console.log('Sending these parameters to function; username: '+ username + ' password: '+ hashedPassword);
+  credentials = {'username': username, 'password': hashedPassword}
+  //console.log('JSON stringify of credentials: ', JSON.stringify(credentials));
+  app.get('/login', async(req, res) => {
     try{
-        const data = await pool.query('SELECT * FROM adminuser');
+      //parameters for calling DB function must be in format username:<username> password:<password> in json format
+        const data = await pool.query(`SELECT api.fn_login(${JSON.stringify(credentials)})`);
         res.json({data});
     }catch (err){
         console.error(err);
         res.status(500).send(err.message);
     }
-});
+  });
+}
 
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
+  loginFunction('Drek', '56789012');
 });
+
+//exporting function, so it can be called in front-end
+module.exports = loginFunction;
