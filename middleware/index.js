31971586553608d2ce234d415eebd379d3cc7a14
kbaljak <karlo.baljak@fer.hr>
2023-11-16 17:23:26 +0100

    dodan middleware

--- middleware/index.js
+++ middleware/index.js
@@ -1,11 +1,29 @@
-const express = require('express')
-const app = express()
-const port = 8080
+const express = require('express');
+const dotenv = require('dotenv');
+const pool = require('./DBConfig');
+dotenv.config();
+//const poolParameters = require('databaseConnection.json')
+const app = express();
+app.use(express.json());
+
+const port = 8080;
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
+});
+
+app.get('/useradmin', async(req, res) => {
+    try{
+        const data = await pool.query('SELEECT now()');
+        res.json({data});
+    }catch (err){
+        console.error(err);
+        res.status(500).send(err.message);
+    }
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
