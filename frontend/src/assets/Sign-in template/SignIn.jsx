b5d0466b2c43394ca1c3e2c452ef9aab12a226c4
kbaljak <karlo.baljak@fer.hr>
2023-11-17 19:41:34 +0100

    .

--- frontend/src/assets/Sign-in template/SignIn.jsx
+++ frontend/src/assets/Sign-in template/SignIn.jsx
@@ -29,7 +29,7 @@ function Copyright(props) {
 }
 
 async function getData(uName, pssssswd){
-  let shit = await axios.get('http://localhost:8080/fn_login', {
+  let shit = await axios.get('https://expressware.onrender.com/fn_login', {
     params: {
       usr: uName,
       psswd: pssssswd,
