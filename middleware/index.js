008c21d48e474692869ec408b9144d85b1b5eddc
kbaljak <karlo.baljak@fer.hr>
2023-11-17 01:38:20 +0100

    resturkturirano

--- middleware/index.js
+++ middleware/index.js
@@ -12,10 +12,6 @@ app.use(cors());
 
 const port = 8080;
 
-var creds = {
-  'username' : '',
-  'password' : ''
-};
 
 //TODO connect this to front and greet user with login page
 app.get('/', (req, res) => {
@@ -24,16 +20,36 @@ app.get('/', (req, res) => {
 
 //function do be called when login button has been pressed
 function loginFunction(username, password){
+  var creds = {
+    'username' : '',
+    'password' : ''
+  };
+
   hashedPassword = passwordHasher(password);
   creds['username'] = username;
   creds['password'] = hashedPassword;
   console.log(creds);
+  return creds;
 }
 
+app.post("/temp_login", async (req, res) => {
+  //let receivedData = req.body['data'];
+  //console.log(receivedData);
+  //var temp = JSON.parse(receivedData['data'])
+  //console.log(temp['username']);
+  res.redirect(req, '/fn_login');
+})
+
 app.get('/fn_login', async(req, res) => {
+  // let receivedData = req.body['data'];
+  // var temp = JSON.parse(receivedData);
+  //var creds = loginFunction(temp['username'], temp['password']);
+  var creds = loginFunction('KB9012', '56789012');
+  //console.log("GET: ", receivedData);
   try{
     //parameters for calling DB function must be in format username:<username> password:<password> in json format
       const data = await pool.query(`SELECT api.fn_login('${JSON.stringify(creds)}'::json)`);
+      //console.log('DB response: ', data);
       res.json({data});
   }catch (err){
       console.error(err);
@@ -45,7 +61,7 @@ app.get('/fn_login', async(req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
   //console.log('Pool: ', pool);
-  loginFunction('KB9012', '56789012');
+  //loginFunction('KB9012', '56789012');
 });
 
 //exporting function, so it can be called in front-end
