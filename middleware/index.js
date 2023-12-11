69ed0a2f8d507b3f1d8c81b9b812a186f98b4d8b
kbaljak <karlo.baljak@fer.hr>
2023-12-11 13:20:46 +0100

    dodana post ruta za dodavanje novog admina

--- middleware/index.js
+++ middleware/index.js
@@ -32,21 +32,43 @@ function loginFunction(username, password){
   return creds;
 }
 
+//Sing into application
 app.get('/fn_login', async(req, res) => {
   const {usr, psswd} = req.query;
   var creds = loginFunction(usr, psswd);
   try{
     //parameters for calling DB function must be in format username:<username> password:<password> in json format
-      //console.log("Creds bedfore call: ", JSON.stringify(creds));
+      //console.log("Creds before call: ", JSON.stringify(creds));
       const data = await pool.query(`SELECT api.fn_login('${JSON.stringify(creds)}'::json)`);
       //console.log('DB: ', data.rows[0]['fn_login']);
       res.json(data.rows[0]['fn_login']);
   }catch (err){
       console.error(err);
-      res.status(500).send(err.message);
+      res.status(400).send(err.message);
   }
 });
 
+//Add new user
+app.post('/fn_add_admin', async(req, res) => {
+  var reqBody = req.body;
+  /* expected body content 
+  {
+    PIN: '',
+    firstname: '',
+    lastname: '',
+    phone: '',
+    email: ''
+  }
+  */
+  try{
+    var response = await pool.query(`SELECT api.fn_add_admin('${JSON.stringify(reqBody)}'::json)`)
+    res.json(response.rows[0]['fn_add_admin']);
+    //console.log(response.rows[0]['add_admin']);
+  }catch (err){
+    res.json(response.rows[0]['fn_add_admin']);
+    res.status(400).send("Bad request");
+  }
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
