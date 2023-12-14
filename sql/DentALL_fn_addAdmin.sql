83bcee0f82ab44dbb1edaab79a30b5b9e32315b5
kbaljak <karlo.baljak@fer.hr>
2023-12-15 00:20:01 +0100

    popravljena funkcija

--- sql/DentALL_fn_addAdmin.sql
+++ sql/DentALL_fn_addAdmin.sql
@@ -1,27 +1,24 @@
-CREATE FUNCTION api.fn_add_admin(IN new_admin_data json)
+CREATE OR REPLACE FUNCTION api.fn_add_admin(IN new_admin_data json)
     RETURNS json
     LANGUAGE 'plpgsql'
     VOLATILE PARALLEL UNSAFE
 AS $BODY$
 declare
-	returnValue json;
+	returnValue json := json_build_object(
+			'success', false,
+			'msg', 'Could not create user'
+		);
 	
 begin
 	if (select count(*) from public.adminuser au where au.pin = (new_admin_data->>'PIN')::integer) = 0 then
 		insert into public.adminuser (pin, firstname, lastname, phone, email)
-		values ((new_admin_data->>'PIN')::integer, (new_admin_data->>'firstname'), (new_admin_data->>'lastname'), (new_admin_data->>'phone'), (new_admin_data->>'email'));
+		values ((new_admin_data->>'PIN')::integer, (new_admin_data->>'firstname')::varchar, (new_admin_data->>'lastname')::varchar, (new_admin_data->>'phone')::varchar, (new_admin_data->>'email')::varchar);
 		
-		select public.fn_create_credentials();
+		perform public.fn_create_credentials();
 		
 		returnValue := json_build_object(
 			'success', true,
 			'msg', 'New admin created'
-		);
-	end if;
-	if returnValue is null then	
-		returnValue := json_build_object(
-			'success', false,
-			'msg', 'Could not create user'
 		);
 	end if;
 	
