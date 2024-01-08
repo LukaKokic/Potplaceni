115d77e92474551dee32adba25ca93a620a16be7
kbaljak <karlo.baljak@fer.hr>
2024-01-08 20:28:47 +0100

    dodana funkcionalnost za dodavanje pacijenta i preferencija

---
+++ sql/DentALL_fn_add_patient.sql
@@ -0,0 +1,62 @@
+-- FUNCTION: api.fn_add_patient(json)
+
+-- DROP FUNCTION IF EXISTS api.fn_add_patient(json);
+
+CREATE OR REPLACE FUNCTION api.fn_add_patient(
+	patient_in json)
+    RETURNS json
+    LANGUAGE 'plpgsql'
+    COST 100
+    VOLATILE PARALLEL UNSAFE
+AS $BODY$
+declare
+	returnValue json := '{"success": false, "msg": "Patient could not be created"}'::json;
+	patient_id integer;
+	pin_in integer := (patient_in->>'pin')::integer;
+	f_name varchar := (patient_in->>'firstname')::varchar;
+	l_name varchar := (patient_in->>'lastname')::varchar;
+	ph varchar := (patient_in->>'phone')::varchar;
+	mail varchar := (patient_in->>'mail')::varchar;
+	home_addr varchar := (patient_in->>'homeAddress')::varchar;
+	acc_type_id integer := (patient_in->>'typePref')::integer;
+	eq_type_id integer := (patient_in->>'equippedPref')::integer;
+	treatment_id integer := (patient_in->>'treatmentID')::integer;
+	treatment_date_from date := (patient_in->>'from')::date;
+	treatment_date_till date := (patient_in->>'till')::date;
+	
+begin
+	if (select count(*) from public.patient where pin = pin_in) then
+		return returnValue;
+	end if;
+	
+	INSERT INTO public.patient (pin, firstname, lastname, phone, email, residenceaddress)
+	VALUES (pin_in, f_name, l_name, ph, mail, home_addr);
+	
+	select
+		patientid
+	into 
+		patient_id
+	from
+		patient
+	order by
+		patientid desc
+	limit 1;
+	
+	INSERT INTO public.patientpreferences (patientid, typeid, equippedid)
+	VALUES (patient_id, acc_type_id, eq_type_id);
+	
+	INSERT INTO public.assigned (treatmentid, patientid, datefrom, dateto)
+	VALUES (treatment_id, patient_id, treatment_date_from, treatment_date_till);
+	
+	returnValue := json_build_object(
+		'success', true,
+		'msg', 'Patient added'
+	);
+	
+	return returnValue;
+	
+end
+$BODY$;
+
+ALTER FUNCTION api.fn_add_patient(json)
+    OWNER TO dentall_rmm2_user;
