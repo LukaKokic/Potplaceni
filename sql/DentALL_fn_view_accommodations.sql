5da6da42bc11e12970337548be5da71c7d7626d1
kbaljak <karlo.baljak@fer.hr>
2024-01-18 12:38:27 +0100

    dodano jos atributa u povratni JSON

--- sql/DentALL_fn_view_accommodations.sql
+++ sql/DentALL_fn_view_accommodations.sql
@@ -1,7 +1,13 @@
-CREATE OR REPLACE FUNCTION api.fn_view_accommodations()
+-- FUNCTION: api.fn_view_accommodations()
+
+-- DROP FUNCTION IF EXISTS api.fn_view_accommodations();
+
+CREATE OR REPLACE FUNCTION api.fn_view_accommodations(
+	)
     RETURNS json
     LANGUAGE 'plpgsql'
-    
+    COST 100
+    VOLATILE PARALLEL UNSAFE
 AS $BODY$
 declare
 	returnValue json;
@@ -14,7 +20,11 @@ begin
 				acc.realestateid realestate_id,
 				acctype.description acc_type,
 				eq.description equipped_type,
-				acc.address || ', ' || t.postalcode || ', ' || t.townname acc_address,
+				acc.address acco_address,
+				acc.latitude acco_lat,
+				acc.longitude acco_long,
+				t.postalcode town_postal,
+				t.townname town_name,
 				acc.active acc_active
 			from
 				accommodation acc
@@ -37,7 +47,11 @@ begin
 			're_id', cte.realestate_id,
 			'acc_type', cte.acc_type,
 			'acc_eq', cte.equipped_type,
-			'address', cte.acc_address,
+			'address', cte.acco_address,
+			'lat', cte.acco_lat,
+			'long', cte.acco_long,
+			'tName', cte.town_name,
+			'tPostal', cte.town_postal,
 			'active', cte.acc_active
 		)
 	)
@@ -50,3 +64,12 @@ $BODY$;
 
 ALTER FUNCTION api.fn_view_accommodations()
     OWNER TO dentall_rmm2_user;
+
+GRANT EXECUTE ON FUNCTION api.fn_view_accommodations() TO PUBLIC;
+
+GRANT EXECUTE ON FUNCTION api.fn_view_accommodations() TO app_api;
+
+GRANT EXECUTE ON FUNCTION api.fn_view_accommodations() TO auth_api;
+
+GRANT EXECUTE ON FUNCTION api.fn_view_accommodations() TO dentall_rmm2_user;
+
