e5820e6a591c608c86e0682e252c55cb6414fc9e
LukaKokic <lk53973@fer.hr>
2024-01-19 15:26:04 +0100

    fixed popup message

--- frontend/src/assets/UserForm.jsx
+++ frontend/src/assets/UserForm.jsx
@@ -61,7 +61,7 @@ const UserForm = ({usersUpdate, roleOptions}) => {
 	{
 		submitForm(formData).then(response => { 
 			console.log("form submitted; response: ", response);
-			window.alert("User created successfully.");
+			window.alert(response.data["msg"]);
 			usersUpdate();
 		});
 	}
@@ -110,9 +110,9 @@ const UserForm = ({usersUpdate, roleOptions}) => {
 								<label htmlFor="admin_prijevoza" className='label'>Administrator prijevoza</label>
 								<input type="checkbox" name="admin_prijevoza" value="2" onClick={handleRoleCheckbox}/>
 							</div>
-							<div name="admin_prijevoza" className="checkbox-div">
-								<label htmlFor="smjestajni_admin" className='label'>Korisnicki administrator</label>
-								<input type="checkbox" name="admin_prijevoza" value="3" onClick={handleRoleCheckbox}/>
+							<div name="korisnicki_admin" className="checkbox-div">
+								<label htmlFor="korisnicki_admin" className='label'>Korisnicki administrator</label>
+								<input type="checkbox" name="korisnicki_admin" value="3" onClick={handleRoleCheckbox}/>
 							</div>
 						</div>
 					  </div>
