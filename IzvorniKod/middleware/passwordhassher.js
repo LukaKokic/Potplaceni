55cd62c7a93d19b688dd9be0c0ca3d8f063b418c
LukaKokic <lk53973@fer.hr>
2024-01-19 23:41:07 +0100

    merge

--- IzvorniKod/middleware/passwordhassher.js
+++
@@ -1,8 +0,0 @@
-const md5 = require('md5');
-
-//user's password is their PIN, which is hashed here using MD5
-function passwordHasher(pass){
-    return md5(pass).substring(0,8);
-}
-
-module.exports = passwordHasher;
